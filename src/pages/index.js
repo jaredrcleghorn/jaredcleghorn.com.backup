f06f660d117c3e38ef9210912931c532db7eb324
Jared Cleghorn <jaredrcleghorn@gmail.com>
2021-02-07 15:58:12 -0600

    Add an "About Me" section

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,15 @@ export default function Home({ data }) {
         <span>&lt;</span>Jared Cleghorn&nbsp;<span>/&gt;</span>
       </h1>
       <main id={styles.main}>
+        <section>
+          <h2>About Me</h2>
+          <p>
+            Hi! My name is Jared Cleghorn&mdash;welcome to my website! I am a
+            student at the University of Alabama pursuing a B.S. with majors in
+            physics and mathematics and a minor in computer science. I also work
+            as a student web developer for the University of Alabama Libraries.
+          </p>
+        </section>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <article key={node.id}>
             <h2>{node.frontmatter.title}</h2>
